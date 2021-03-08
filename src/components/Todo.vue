ecacaee2ad5e5e2d90588d79fa396d741023a9e8
Kaan Yilmaz <douscriptist@gmail.com>
2021-03-08 23:59:36 +0300

    change

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -14,4 +14,23 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.todo h4 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-weight: 500;
+}
+
+.todo {
+  background: rgb(251, 244, 244);
+  margin: 5px;
+  padding: 10px 20px;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+.todo.reminder {
+  border-left: 6px solid greenyellow;
+}
+</style>
